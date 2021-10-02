3bb853a491ced2bc6b2f9aa55a2e9f6f3602f862
jangThang <star7sss@naver.com>
2021-10-03 05:02:55 +0900

    MODIFY Drawpath method

--- app/src/main/java/com/example/myfirstgooglemap/MapsActivity.java
+++ app/src/main/java/com/example/myfirstgooglemap/MapsActivity.java
@@ -21,7 +21,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -36,8 +35,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.io.*;
 import java.util.*;
@@ -61,9 +60,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ArrayList<Marker> markers_disabled, markers_smoking,markers_building;
 
     private static final int NODE = 43; // 노드(학교 장소) 갯수
-    public ArrayList<Vertex> vertex;       // vertex 객체배열
+    public static ArrayList<Vertex> vertex = new ArrayList<>(NODE); // vertex 객체배열
 
-    private PolylineOptions polylineOptions = new PolylineOptions();
+    public int convert(String spot) {
+        int result = -1; // 못 찾았을 경우, -1로 반환합니다.
+
+        // 건물이름으로 찾습니다.
+        for(int i = 0; i < NODE; i++) {
+            if(spot.equals(vertex.get(i).name) | spot.equals(vertex.get(i).name_eng)) {
+                return i;
+            }
+        }
+        return result;
+    }
+
+    // 길찾기 버튼 클릭 리스너 추가바람
+    img_search.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            // 출발지랑 도착지 넣는 텍스트 뷰에서 값 가져오기
+            int start = convert(autotext_building.getText().toString()); // 선택한 출발지를 객체배열의 고유번호로 바꿔줍니다.
+            int end = convert(autotext_building.getText().toString()); // 선택한 도착지를 객체배열의 고유번호로 바꿔줍니다.
+
+            Daijkstra path = Daijkstra.getInstance(); // 다익스트라 singleton 객체 Path를 생성합니다.
+
+            try {
+                setVertex(); // vertex 초기화
+                path.calDaijkstra(FALSE, vertex); // 경로 계산
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            String[] pathNode = path.getPathNode().split(" ", 0); // 계산된 경로(String)을 연산을 위해 배열로 만듭니다.
+
+            // 캠퍼스지도상에 그릴 polyLine객체를 pathNode를 토대로 생성합니다.
+            for(int i = 1; i < pathNode.length; i++) {
+                int vertexNum = Integer.parseInt(pathNode[i]);
+                // polyline 그리는 코드
+                mMap.addPolyline((new PolylineOptions()).add(new LatLng(vertex.get(vertexNum-1).latitude, vertex.get(vertexNum-1).longitude),
+                        new LatLng(vertex.get(vertexNum).latitude, vertex.get(vertexNum).longitude)).width(5).color(Color.RED).geodesic(TRUE));
+            }
+
+            // 건물 고유숫자로 된 경로를 건물명으로 바꿉니다.
+            String pathInfo = "";
+            for(int i = 0; i < pathNode.length; i++) {
+                pathInfo += vertex[Integer.parseInt(pathNode[i])].name;
+                if(i == pathNode.length-1)
+                    break;
+                pathInfo += " -> ";
+            }
+
+            /* 있으면 좋을듯한 추가기능
+            int minute = path.getTime() / 60;
+            int second = path.getTime() % 60;
+
+            pathTf.setText("경로 : " + pathInfo + " (총 " + path.getNodeCount() + "곳 지남)"); // 경로 표시
+            meterTf.setText("거리 : " + path.getMeter() + "m"); // 거리 표시
+            timeTf.setText("예상 소요시간 : " + minute + "분 " + second + "초"); //
+            BorderPane textPn = new BorderPane();
+            pathTf.setPrefWidth(700);
+            meterTf.setPrefWidth(100);
+            timeTf.setPrefWidth(200);
+
+            textPn.setTop(pathTf);
+            textPn.setLeft(meterTf);
+            textPn.setCenter(timeTf);
+             */
+        }
+    });
+
 
     private static final double[] DISABLED_PARKING_POINTS = {
             37.635782, 127.076478,   // 성림학사
@@ -172,8 +236,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //LatLng 값 할당
         center = new LatLng(37.632503, 127.078933);
 
-        polylineOptions.color(Color.RED);
-
         // 뷰 할당
         imgbtn_no  = mapFragment.getView().findViewById(R.id.btn_no);
         imgbtn_disabled = mapFragment.getView().findViewById(R.id.btn_disabled);
@@ -202,8 +264,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         markers_disabled = new ArrayList<>();
         markers_smoking = new ArrayList<>();
         markers_building = new ArrayList<>();
-        vertex = new ArrayList<>();
-
+        
         // Vertex 초기화
         try {
             setVertex();
@@ -253,69 +314,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         layout_bottom_btns.setVisibility(View.GONE);
 
                         // TODO 검색어에 따라서 패널에 정보 추가 하기
-
-
-                        // TODO 카메라 줌 인
-
+                        
+                        
                         break;
                     }
                 }
 
             }
         });
-        /*
-        img_search.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                // 출발지랑 도착지 넣는 텍스트 뷰에서 값 가져오기
-                int start = convert(autotext_building.getText().toString()); // 선택한 출발지를 객체배열의 고유번호로 바꿔줍니다.
-                int end = convert(end???.getText().toString()); // 선택한 도착지를 객체배열의 고유번호로 바꿔줍니다.
 
-                Daijkstra path = Daijkstra.getInstance(); // 다익스트라 singleton 객체 Path를 생성합니다.
-
-                try {
-                    setVertex(); // vertex 초기화
-                    path.calDaijkstra(FALSE, vertex); // 경로 계산
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                String[] pathNode = path.getPathNode().split(" ", 0); // 계산된 경로(String)을 연산을 위해 배열로 만듭니다.
-
-                // 캠퍼스지도상에 그릴 polyLine객체를 pathNode를 토대로 생성합니다.
-                for(int i = 1; i < pathNode.length; i++) {
-                    System.out.println(pathNode[i-1]);
-                    // polyline 그리는 코드
-                }
-
-                // 건물 고유숫자로 된 경로를 건물명으로 바꿉니다.
-                String pathInfo = "";
-                for(int i = 0; i < pathNode.length; i++) {
-                    pathInfo += vertex.get(Integer.parseInt(pathNode[i])-1).name;
-                    if(i == pathNode.length-1)
-                        break;
-                    pathInfo += " -> ";
-                }
-
-            /* 있으면 좋을듯한 추가기능
-            int minute = path.getTime() / 60;
-            int second = path.getTime() % 60;
-
-            pathTf.setText("경로 : " + pathInfo + " (총 " + path.getNodeCount() + "곳 지남)"); // 경로 표시
-            meterTf.setText("거리 : " + path.getMeter() + "m"); // 거리 표시
-            timeTf.setText("예상 소요시간 : " + minute + "분 " + second + "초"); //
-            BorderPane textPn = new BorderPane();
-            pathTf.setPrefWidth(700);
-            meterTf.setPrefWidth(100);
-            timeTf.setPrefWidth(200);
-
-            textPn.setTop(pathTf);
-            textPn.setLeft(meterTf);
-            textPn.setCenter(timeTf);
-
-            }
-        });
-
-             */
     }
 
     @Override
@@ -384,34 +391,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
-    public void setVertex() throws IOException{
-        try {
-            InputStream is = this.getApplicationContext().getResources().openRawResource(R.raw.vertex);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String line;
+    public static void setVertex() throws IOException{
+        File vertexFile = new File("vertex.txt");
+        // 한줄씩 위도, 경도, 건물번호, 이름, 영어이름, 흡연구역 유무, 장애인주차구역 유무, 경사로 유무
 
-            int vertexNum = 0;
-
-            while ((line = reader.readLine()) != null) {
-                String[] st = line.split(" ");
-
-                Vertex v = new Vertex(Double.parseDouble(st[0]),
-                        Double.parseDouble(st[1]),
-                        Integer.parseInt(st[2]),
-                        st[3],
-                        st[4],
-                        Integer.parseInt(st[5]) == 1,
-                        Integer.parseInt(st[6]) == 1,
-                        Integer.parseInt(st[7]) == 1
-                        );
-                vertex.add(v);
-
-                vertexNum++;
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+        // 해당 파일이 없을 경우, 예외처리
+        if (!vertexFile.exists()) {
+            System.out.println("vertex 파일이 존재하지 않습니다.");
+            //System.exit(2);
+            throw new IOException("vertex 파일이 존재하지 않습니다");
         }
+
+        Scanner input = new Scanner(vertexFile);
+        int vertexNum = 0;
+
+        // Vertex파일을 읽어, vertex 객체배열을 초기화합니다.
+        while(input.hasNext()){
+            StringTokenizer st = new StringTokenizer(input.nextLine());
+
+            vertex[vertexNum].latitude = Double.parseDouble(st.nextToken());
+            vertex[vertexNum].longitude = Double.parseDouble(st.nextToken());
+            vertex[vertexNum].id = Integer.parseInt(st.nextToken());
+            vertex[vertexNum].name = st.nextToken();
+            vertex[vertexNum].name_eng = st.nextToken();
+            vertex[vertexNum].is_smoke = Integer.parseInt(st.nextToken()) == 1;
+            vertex[vertexNum].is_disabled = Integer.parseInt(st.nextToken()) == 1;
+            vertex[vertexNum].is_slope = Integer.parseInt(st.nextToken()) == 1;
+
+            vertexNum++;
+        }
+        input.close();
     }
 
     private void HideKeyboard(){
@@ -420,17 +429,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
-    }
-
-    public int convert(String spot) {
-        int result = -1; // 못 찾았을 경우, -1로 반환합니다.
-
-        // 건물이름으로 찾습니다.
-        for(int i = 0; i < NODE; i++) {
-            if(spot.equals(vertex.get(i).name) | spot.equals(vertex.get(i).name_eng) | spot.equals(Integer.toString(vertex.get(i).id))) {
-                return i;
-            }
-        }
-        return result;
     }
 }
